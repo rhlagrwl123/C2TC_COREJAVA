421527f1e87decc767698786aa318a160e3605bd
Rahul <Rahul@DESKTOP-R6M4SI1>
2022-04-29 13:54:47 +0530

    Revert "commit"
    This reverts commit 1d91df878ceb84447f879cdea4df052134e7838a.

--- MoneyMoneyBank/src/Client/Client.java
+++ MoneyMoneyBank/src/Client/Client.java
@@ -1,7 +1,5 @@
 package Client;
 
-import java.util.Scanner;
-
 import com.tnsif.application.*;
 import com.tnsif.framework.*;
 
@@ -11,17 +9,7 @@ public class Client {
 		SavingAcc sa = new MMSavingAcc(12452, "Rahul Agrawal", 4586521, true);
 		CurrentAcc ca = new MMCurrentAcc(25754, "Mohit Patel", 45865, 500000);
 		
-		
-		float accBal = sa.getAccBal();
-		String accNm = sa.getAccNm();
-		int accNo = sa.getAccNo();
-		System.out.println("Your saving account no. is :"+accNo);
-		System.out.println("account name is :"+accNm);
-		System.out.println("your account Balance is :"+accBal);
-		System.out.println("Enter your amount to withdraw from your account...");
-		Scanner sc = new Scanner(System.in);
-		int amt = sc.nextInt();
-		sa.withdraw(amt);
+		sa.withdraw(488744);
 		
 	}
 
